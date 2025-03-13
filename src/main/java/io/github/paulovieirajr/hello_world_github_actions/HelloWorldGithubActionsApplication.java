4c86f0506ddef9e0954b4aa4eb9e0d634db92196
Paulo Vieira <paulovieiradev@outlook.com>
2025-03-13 12:12:27 -0300

    feat: Added Spring Boot components

---
+++ src/main/java/io/github/paulovieirajr/hello_world_github_actions/HelloWorldGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package io.github.paulovieirajr.hello_world_github_actions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/paulovieirajr/hello_world_github_actions/HelloWorldGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.paulovieirajr.hello_world_github_actions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
