2f01d35f777b3faba2f421257dd154f954d128cb
Paulo Vieira <paulovieiradev@outlook.com>
2025-03-13 12:27:33 -0300

    feat: Added hello endpoint

--- src/main/java/io/github/paulovieirajr/hello_world_github_actions/HelloWorldGithubActionsApplication.java
+++ src/main/java/io/github/paulovieirajr/hello_world_github_actions/HelloWorldGithubActionsApplication.java
@@ -2,12 +2,19 @@ package io.github.paulovieirajr.hello_world_github_actions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HelloWorldGithubActionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldGithubActionsApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
 }
